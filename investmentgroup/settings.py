8096d30c0be9eee232a77280938c357ecf8333cf
ADIT IBM <jiteshmohapatra2000@gmail.com>
2023-09-07 11:21:23 +0530

    added configuration for vercel

--- investmentgroup/settings.py
+++ investmentgroup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-bn(dbf+@vh$lqc=sgq6^80$ksv8)l!te9%rugtx*8z#hz_)u)2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -77,9 +78,13 @@ WSGI_APPLICATION = 'investmentgroup.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "projectdjango",
+        "USER": "finance",
+        "PASSWORD": "Nsti123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
@@ -119,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
